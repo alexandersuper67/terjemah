16449db1cb25852eda9db108b8300a516a3e6c81
alexandersuper67 <43996487+alexandersuper67@users.noreply.github.com>
2018-10-15 22:58:24 +0700

    Add files via upload

--- languageSupport.php
+++ languageSupport.php
@@ -11,16 +11,20 @@ switch ($tujuan) {
     break;
 
   case 'belanda':
+  case 'Belanda':
   case 'netherland':
     $kode_bahasa = 'nl';
     break;
 
     case 'italia':
+    case 'Italia':
     case 'itali':
       $kode_bahasa = 'it';
       break;
 
       case 'cina':
+      case 'Cina':
+      case 'China':
       case 'china':
       case 'chinese':
       case 'mandarin':
@@ -28,6 +32,7 @@ switch ($tujuan) {
         break;
 
         case 'korea':
+        case 'Korea':
         case 'hangul':
         case 'hanggul':
         case 'korsel':
@@ -35,17 +40,20 @@ switch ($tujuan) {
           break;
 
           case 'jepang':
+          case 'Jepang':
           case 'japan':
           case 'japanese':
           $kode_bahasa ='ja';
           break;
 
           case 'jerman':
+          case 'Jerman':
           case 'germany':
           $kode_bahasa = 'de';
           break;
 
           case 'perancis':
+          case 'Perancis':
           case 'france':
           case 'prancis':
           case 'french':
@@ -53,37 +61,43 @@ switch ($tujuan) {
           break;
 
           case 'thailand':
+          case 'Thailand':
           case 'thai':
           $kode_bahasa ='th';
           break;
 
           case 'hindi':
+          case 'Hindi':
           case 'india':
           case 'hindia':
           $kode_bahasa = 'hi';
           break;
 
           case 'spanyol':
+          case 'Spanyol':
           case 'spain':
           case 'spanish':
           $kode_bahasa = 'es';
           break;
 
           case 'rusia':
+          case 'Rusia':
           case 'russia':
           case 'russian':
           $kode_bahasa = 'ru';
           break;
 
           case 'arab':
+          case 'Arab':
           case 'arabic':
           case 'arabian':
           $kode_bahasa='ar';
           break;
 
-          case 'javanese';
-          case 'java';
-          case 'jawa';
+          case 'javanese':
+          case 'Jawa':
+          case 'java':
+          case 'jawa':
           $kode_bahasa='jv';
           break;
 
