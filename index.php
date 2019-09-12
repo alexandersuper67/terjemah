bc50e7e26ed87ea56ead1e2ae3f57204ae790140
alexandersuper67 <43996487+alexandersuper67@users.noreply.github.com>
2019-09-11 23:33:55 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,43 +1,38 @@
 <?php
 $method = $_SERVER['REQUEST_METHOD'];
-// Process only when method is POST
+// iki proses method
 if($method == 'POST'){
-  // medapatkan hasil dari dialogflow
+  // njaluk hasil teko dialogflow
   $requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
   $kalimat = $json->result->parameters->kalimat;
   $tujuan =  $json->result->parameters->tujuan;
-
-  //mengubah teks biasa ke urlencode
+  //ngubah tulisan nyang urlencode
   $text = urlencode($kalimat);
-
-
-  //kunci dari yandex
+  //kunci teko yandex
   $key =  'trnsl.1.1.20181008T102144Z.3cfd726c2fdb63bd.f03e7ed0d0a60d29312774934c2e8532bbc47950';
   //dukungan basasa
 		include 'languageSupport.php';
-
-//kirim data ke yandex
+//ngirim data nyang yandex
 		$language_from = "https://translate.yandex.net/api/v1.5/tr.json/detect?key=$key&text=$text";
-//dapat balasan dari yandex
+//oleh balasan teko yandex
     $url_from = file_get_contents($language_from);
-//deteksi awal bahasa
+//deteksi bahasa awal
 		$obj_language_from = json_decode($url_from);
 		$asal = $obj_language_from->lang;
-
 //jawaban dukungan bahasa
 		if($kode_bahasa == 'null'){
-      //kalau bahasa tidak di dukung
-			$hasil = 'sorry, aku belum bisa kalau pake bahasa itu, mungkin besok udah bisa :v';
+      //lek bahasa ne gak onok
+			$hasil = 'Maaf ya :( Aku belum bisa bahasa itu mungkin besok aku sudah bisa :)';
 		}else {
-      // kalau bahasa di dukung
-      //kirim data ke yandx
+      // lek bahasa ne onok
+      //kirim data nyang yandex
 			$situs = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=$key&text=$text&lang=$asal-$kode_bahasa";
-      //dapat balasan
+      //oleh balasan
       $url = file_get_contents($situs);
-      //ubah ke json
+      //ubah nyang json
       $obj = json_decode($url);
-			// dapat hasiknya
+			// oleh hasil e
 			$hasil = $obj->text[0];
 		}
 
